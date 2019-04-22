7d205bbba783bf4ca02ca6d698fcfb7bb2015afb
adhuper <40460022+adhuper@users.noreply.github.com>
2019-04-22 15:26:57 +0530

    Add files via upload

--- docLauncher/src/com/doc/launcher/core/DocLauncher.java
+++ docLauncher/src/com/doc/launcher/core/DocLauncher.java
@@ -2,17 +2,17 @@ package com.doc.launcher.core;
 
 import java.awt.Desktop;
 import java.io.File;
-import java.io.IOException;
+
+import javax.swing.JOptionPane;
 
 public class DocLauncher {
 
 	public static void main(String args[]) {
 		if(Desktop.isDesktopSupported()) {
 			try {
-				Desktop.getDesktop().open(new File("c:\\MSDE\\Template.docx"));
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				Desktop.getDesktop().open(new File(args[0]));
+			} catch (Exception e) {
+				JOptionPane.showMessageDialog(null, "Error in opening file", "Error", JOptionPane.ERROR_MESSAGE);
 			}
 		}
 	}
